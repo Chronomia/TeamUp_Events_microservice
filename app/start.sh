07aede6fda714f567d3215df983e7d584ee574aa
Chronomia <48509528+Chronomia@users.noreply.github.com>
2023-11-18 14:18:41 -0500

    Add files via upload

---
+++ app/start.sh
@@ -0,0 +1,6 @@
+export AWS_ACCESS_KEY_ID="AKIARBMGSPSUEQDJC3PA"
+export AWS_SECRET_ACCESS_KEY="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"
+export AWS_DEFAULT_REGION="us-east-1"
+
+python3 ./src/initialize.py
+python3 main.py
