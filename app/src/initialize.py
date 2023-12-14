3bd3f368598300abfbe5c6cc6cf4ac3f39d35c87
Chronomia <48509528+Chronomia@users.noreply.github.com>
2023-12-14 13:16:52 -0500

    Update initialize.py

--- app/src/initialize.py
+++ app/src/initialize.py
@@ -5,7 +5,7 @@ from models import Event, Group, Member, Comment
 # Initialize DynamoDB Client
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 
-tables_to_delete = ["Event", "Group", "Member", "Comment"]
+tables_to_delete = ["Event", "Group", "Comment", "EventMemberRelation"]
 
 # Function to delete a table
 def delete_table(table_name):
@@ -43,22 +43,15 @@ def create_table(name, key_schema, attribute_definitions, read_capacity_units=1,
 # Event Table
 create_table(
 	name="Event",
-	key_schema=[{'AttributeName': 'event_id', 'KeyType': 'HASH'}],  # Partition key
+	key_schema=[{'AttributeName': 'event_id', 'KeyType': 'HASH'}],  
 	attribute_definitions=[{'AttributeName': 'event_id', 'AttributeType': 'N'}]
 )
 
 # Group Table
 create_table(
 	name="Group",
-	key_schema=[{'AttributeName': 'group_id', 'KeyType': 'HASH'}],  # Partition key
+	key_schema=[{'AttributeName': 'group_id', 'KeyType': 'HASH'}], 
 	attribute_definitions=[{'AttributeName': 'group_id', 'AttributeType': 'N'}]
-)
-
-# Member Table
-create_table(
-	name="Member",
-	key_schema=[{'AttributeName': 'user_id', 'KeyType': 'HASH'}], 
-	attribute_definitions=[{'AttributeName': 'user_id', 'AttributeType': 'N'}]
 )
 
 # Comment Table
@@ -68,19 +61,40 @@ create_table(
 	attribute_definitions=[{'AttributeName': 'comment_id', 'AttributeType': 'N'},  {'AttributeName': 'event_id', 'AttributeType': 'N'}]
 )
 
+# EventMemberRelation Table 
+create_table(
+	name="EventMemberRelation"
+	key_schema=[{'AttributeName': 'event_id', 'KeyType': 'HASH'}, {'AttributeName': 'user_id', 'KeyType': 'RANGE'}],  # Partition key
+	attribute_definitions=[{'AttributeName': 'user_id', 'AttributeType': 'N'},  {'AttributeName': 'event_id', 'AttributeType': 'N'}]
+)
+
+
 
 # Function to Load Data into DynamoDB
 def load_data_to_dynamodb(table_name, data):
 	table = dynamodb.Table(table_name)
 	for item in data:
 		table.put_item(Item=item.model_dump())
+
+def load_event_to_dynamodb(csv_file):
+    table = dynamodb.Table("Event")
+    event_data = pd.read_csv(csv_file)
+    for _, row in event_data.iterrows():
+        event = Event(
+            event_id=row['event_id'],
+            status=row['status'],
+            capacity=row['capacity'],
+            event_name=row['event_name'],
+            description=row['description'],
+            location=row['location'],
+            time=row['time'],
+            group_id=row.get('group_id', ''),
+            organizer_id=row.get('organizer_id', ''),
+            tag_1=row['tag_1'],
+            tag_2=row.get('tag_2', '')
+        )
+        table.put_item(Item=event.model_dump())
 		
-events = [
-	Event(event_id=1, group_id=101, attended_person=[1, 2], not_attended_person=[3], no_response_person=[4, 5]),
-	Event(event_id=2, group_id=102, attended_person=[6, 7], not_attended_person=[8], no_response_person=[9, 10]),
-	Event(event_id=3, group_id=103, attended_person=[11, 12], not_attended_person=[13], no_response_person=[14, 15]),
-	# Add more events as needed
-]
 
 groups = [
 	Group(group_id=101, name="Group 1", description="Description for Group 1"),
@@ -88,21 +102,7 @@ groups = [
 	Group(group_id=103, name="Group 3", description="Description for Group 3"),
 	Group(group_id=104, name="Group 4", description="Description for Group 4"),
 	Group(group_id=105, name="Group 5", description="Description for Group 5"),
-	Group(group_id=106, name="Group 6", description="Description for Group 6"),
-	Group(group_id=107, name="Group 7", description="Description for Group 7"),
-	Group(group_id=108, name="Group 8", description="Description for Group 8"),
-	Group(group_id=109, name="Group 9", description="Description for Group 9"),
-	Group(group_id=110, name="Group 10", description="Description for Group 10")
 	# Add more groups as needed
-]
-
-members = [
-	Member(user_id=1, username="User1", attended_events=[1], attended_groups=[101, 102]),
-	Member(user_id=2, username="User2", attended_events=[1], attended_groups=[102]),
-	Member(user_id=3, username="User3", attended_events=[2], attended_groups=[103]),
-	Member(user_id=4, username="User4", attended_events=[2], attended_groups=[101, 103]),
-	Member(user_id=5, username="User5", attended_events=[3], attended_groups=[104]),
-	# Add more members as needed
 ]
 
 comments = [
@@ -130,7 +130,8 @@ def scan_all_events():
 
 # Load Data into DynamoDB
 if __name__ == "__main__":
-	load_data_to_dynamodb('Event', events)
+	# load_data_to_dynamodb('Event', events)
+	load_event_to_dynamodb("./mock_data.csv")
 	load_data_to_dynamodb('Group', groups)
 	load_data_to_dynamodb('Member', members)
 	load_data_to_dynamodb('Comment', comments)
